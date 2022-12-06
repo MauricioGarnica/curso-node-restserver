5834f9b78d19f045692d35b9c04818b85711408b
Mauricio Garnica <edgarmauriciog99@gmail.com>
2022-12-06 12:04:49 -0600

    Primer commit

---
+++ models/server.js
@@ -0,0 +1,44 @@
+import {router} from '../routes/usuarios.js'
+import express from 'express';
+import cors from 'cors';
+
+class Server{
+    constructor(
+        
+    ){
+        this.app = express()
+        this.port = process.env.PORT
+        this.usuariosPath = '/api/usuarios'
+
+        //Middlewares
+        this.middlewares();
+
+        //Rutas de la aplicacion
+        this.routes();
+    }
+
+    middlewares(){
+        //CORS
+        this.app.use(cors());
+
+        //Lectura y parseo del body
+        this.app.use(express.json());
+
+        //Directorio publico
+        this.app.use(express.static('public'));
+    }
+
+    routes(){
+        this.app.use('/api/usuarios', router);
+    }
+
+    listen(){
+        this.app.listen(this.port, () => {
+            console.log('Servidor corriendo en: ', this.port);
+        });
+    }
+}
+
+export {
+    Server
+}
